23dab866a9df667f788505442937487e900a98c2
Viktor Richter <vrichter@posteo.de>
2015-09-16 12:25:46 +0200

    Add some tests to test.cpp

--- test.cpp
+++ test.cpp
@@ -22,7 +22,7 @@
 
 #include "hungarian.hpp"
 
-std::vector< std::vector<int> > array_to_matrix(int* m, int rows, int cols) {
+std::vector< std::vector<int> > array_to_matrix(const int* m, int rows, int cols) {
   int i,j;
   std::vector< std::vector<int> > r;
   r.resize(rows, std::vector<int>(cols, 0));
@@ -35,31 +35,87 @@ std::vector< std::vector<int> > array_to_matrix(int* m, int rows, int cols) {
   return r;
 }
 
-
-int main() {
-
-  /* an example cost matrix */
-  int r[4*3] =  {   100,   100,     1,
-                    100,     2, 21512,
-                      1,     4,  9852,
-                      6, 30252,   400 };
-  std::vector< std::vector<int> > m = array_to_matrix(r,4,3);
+void do_test(const int* cost_matrix, int rows, int columns,
+             const int* expected_assignment,
+             bool verbose=1)
+{
+  std::vector<std::vector<int> > m = array_to_matrix(cost_matrix,4,3);
 
   /* initialize the gungarian_problem using the cost matrix*/
   Hungarian hungarian(m , 4,3, HUNGARIAN_MODE_MINIMIZE_COST) ;
-
-  //fprintf(stderr, "assignement matrix has a now a size %d rows and %d columns.\n\n",  hungarian.ro,matrix_size);
-
-  /* some output */
-  fprintf(stderr, "cost-matrix:");
-  hungarian.print_cost();
-
-  /* solve the assignement problem */
+  if(verbose){
+    std::cerr << "Created cost matrix:\n";
+    hungarian.print_cost();
+  }
   hungarian.solve();
+  if(verbose){
+    std::cerr << "Calculated Assignment matrix:\n";
+    hungarian.print_assignment();
+  }
+  const std::vector<std::vector<int> >& assignment = hungarian.assignment();
+  const int* expected_assignment_position = expected_assignment;
+  for(unsigned int row = 0; row < rows; ++row){
+    for (unsigned int column = 0; column < columns; ++column, ++expected_assignment_position){
+      if(assignment[row][column] != *expected_assignment_position){
+        std::cerr << "Test failed for cost matrix:\n";
+        hungarian.print_cost();
+        std::cerr << "Assignment:\n";
+        hungarian.print_assignment();
+        std::cerr << "Assignment in row = " << row << " column = " << column << " not expected." << std::endl;
+        return;
+      }
+    }
+  }
+  std::cerr << "Test passed." << std::endl;
+}
 
-  /* some output */
-  fprintf(stderr, "assignment:");
-  hungarian.print_assignment();
+int main() {
+
+  {
+  // test 1
+  std::cerr << "Test #" << 1 << std::endl;
+  int cost[4*3] =  {   100,   100,     1,
+                       100,     2, 21512,
+                         1,     4,  9852,
+                         6, 30252,   400 };
+
+  int assign[4*3] = {    0,     0,     1,
+                         0,     1,     0,
+                         1,     0,     0,
+                         0,     0,     0};
+
+  do_test(cost,4,3,assign,false);
+  }
+
+  {
+  // test 2
+  std::cerr << "Test #" << 2 << std::endl;
+  int cost[2*2] =  { 2, 0,
+                     1, 0 };
+
+  int assign[2*2] = { 0, 0,
+                      0, 1 };
+
+
+  do_test(cost,2,2,assign,false);
+  }
+
+  {
+  // test 3
+  std::cerr << "Test #" << 3 << std::endl;
+  int cost[4*4] = {20,20,10,10,
+                   10,10,30,20,
+                   10,10,10,20,
+                   20,20,30,10};
+
+  int assign[4*4] = {0,0,1,0,
+                     1,0,0,0,
+                     0,1,0,0,
+                     0,0,0,1};
+
+  do_test(cost,4,4,assign,true);
+  }
+
 
 
   return 0;
